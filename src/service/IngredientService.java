19d63d20c4e4de776bef87ab1f1dec3d3cf17768
Дом <ira-rostov80@mail.ru>
2023-01-08 19:38:26 +0300

    первый коммит

---
+++ src/service/IngredientService.java
@@ -0,0 +1,35 @@
+package me.recipe.homework.service;
+
+import me.recipe.homework.model.Ingredient;
+import me.recipe.homework.model.Recipe;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Service
+
+public class IngredientService {
+    private final Map<Long, Ingredient> ingredients = new HashMap<>();
+    private static long lastId = 0;
+
+    public Ingredient addIngredient(Ingredient ingredient) {
+        if (ingredients.containsKey(ingredient.getId())) {
+            throw new RuntimeException("Такой ингредиент уже есть");
+        } else {
+            ingredients.put(lastId++, ingredient);
+        }
+        return ingredient;
+    }
+
+    public Ingredient getById (String id){
+        if (ingredients.containsKey(id)){
+            return ingredients.get(id);}
+        else {
+            throw new RuntimeException("Такого инредиента нет");
+        }
+    }
+}
+}
---
+++ src/service/RecipeService.java
@@ -0,0 +1,34 @@
+package me.recipe.homework.service;
+
+import me.recipe.homework.model.Recipe;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+
+public class RecipeService {
+    private final Map<Long, Recipe> recipes = new HashMap<Long, Recipe>();
+    private static long lastId = 0;
+
+    public Recipe addRecipe(Recipe recipe) {
+        if (recipes.containsKey(recipe.getId())) {
+            throw new RuntimeException("Такой рецепт уже есть");
+        } else {
+            recipes.put(lastId++, recipe);
+        }
+        return recipe;
+    }
+    public Recipe getById (String id){
+        if (recipes.containsKey(id)){
+            return recipes.get(id);}
+        else {
+            throw new RuntimeException("Такого рецепта нет");
+        }
+    }
+
+    public Collection<me.recipe.homework.model.Recipe> getAll() {
+        return null;
+    }
+}
